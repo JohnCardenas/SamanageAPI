914bfb2e4eec28fc02969db1addf335fd33b749b
JohnCardenas <Cardenas.John.J@gmail.com>
2017-11-13 17:13:08 -0800

    Mark required Category field

--- SamanageAPI/Category.cs
+++ SamanageAPI/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,8 @@ namespace SamanageAPI
         #endregion // Fields
 
         #region Properties
+        [Required]
+        [JsonRequired]
         [JsonProperty(JSON_NAME)]
         public string Name
         {
