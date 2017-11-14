530a3b55e508ffbfcb7e0a83142caa3b93ae88c7
JohnCardenas <Cardenas.John.J@gmail.com>
2017-11-13 16:53:33 -0800

    Add interface for future Mock

--- SamanageAPI/SamanageClient.cs
+++ SamanageAPI/SamanageClient.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SamanageAPI.UnitTesting;
 
 namespace SamanageAPI
 {
-    public class SamanageClient
+    public class SamanageClient : ISamanageClient
     {
         public bool Commit(SamanageObject obj)
         {
